ab6b1c2d71184068e9585ffc6ca601a1365e3d89
Abdulla <abdullahayitboyev0408@gmail.com>
2024-02-26 15:37:09 +0500

    main.py faylini qo'shdim

---
+++ main.py
@@ -0,0 +1,29 @@
+
+import time
+def calculate(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        res= func(*args, **kwargs)
+        print(f"{time.time() - start} vaqt ketti!")
+        return res
+    return wrapper
+
+
+@calculate
+def start():
+    users = []
+    while True:
+        username = input("Enter username: ")
+        if username == "stop":
+            break
+        users.append(username)
+    return users
+print(start())
+
+# @calculate
+# def multiplay(x,y):
+#     for i in range(5000000000000000000):
+#         pass
+#     return x * y
+
+# print(multiplay(4,5))
